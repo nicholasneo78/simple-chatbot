4f640bbd3c07ec3edf43840d6b5a385f4f4de773
Nicholas Neo <nicholasneo78@gmail.com>
2021-07-06 12:59:14 +0800

    remove testing codes

--- nltk_utils.py
+++ nltk_utils.py
@@ -10,7 +10,6 @@ import numpy as np
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
-
 # run for the first time
 #nltk.download('punkt')
 
@@ -21,14 +20,6 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    """
-    return bag of words array:
-    1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
-    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
-    """
     tokenized_sentence = [stem(w) for w in tokenized_sentence]
     bag = np.zeros(len(all_words), dtype=np.float32)
     for idx, w in enumerate(all_words):
@@ -36,19 +27,4 @@ def bag_of_words(tokenized_sentence, all_words):
             bag[idx] = 1.0
     return bag
 
-''' test code
-a = "How long does shipping take?"
-print(a)
-a = tokenize(a)
-print(a)
-
-words = ['Organize', 'organizes', 'organiing']
-stemmed_words = [stem(w) for w in words]
-print(stemmed_words)
-
-sentence = ["hello", "how", "are", "you"]
-words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-bag = bag_of_words(sentence, words)
-print(bag)
-'''
 
